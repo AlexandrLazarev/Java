1e708725334f0a91dab1657e32c9695e64c2117f
AlexandrLazarev <lazarev99@gmail.com>
2022-01-27 21:44:52 +0300

    modified

--- src/Task260122/Task3.java
+++ src/Task260122/Task3.java
@@ -4,14 +4,11 @@ public class Task3 {
     public static void main(String[] args) {
         int g = 1;
         int[] array = {2, 5, 6, 9, 23, 56};
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int num = array[i];
             array[i] = array[array.length - g];
             array[array.length - g] = num;
             g++;
-            if (i >= array.length / 2) {
-                break;
-            }
         }
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
