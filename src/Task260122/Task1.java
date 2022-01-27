2a9800b36e43e6c0638818624a13731f29aa847c
AlexandrLazarev <lazarev99@gmail.com>
2022-01-27 21:36:15 +0300

    modified

--- src/Task260122/Task1.java
+++ src/Task260122/Task1.java
@@ -2,15 +2,16 @@ package Task260122;
 
 public class Task1 {
     public static void main(String[] args) {
-        int n = 9;
+        int n = 4;
         int result = 0;
         int[] array = {4, 6, 9, 12, 25};
         for (int i : array) {
-            result += i;
             if (i == n) {
-                result -= i;
+                continue;
             }
+            result += i;
         }
         System.out.println("Сумма чисел массива без числа \"n\" равна " + result);
     }
 }
+
