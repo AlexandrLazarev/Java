9bd99079cd768f3523b3881b58fe821d87cca2e6
AlexandrLazarev <lazarev99@gmail.com>
2022-02-01 18:57:50 +0300

    changing

--- src/Task260122/Task7.java
+++ src/Task260122/Task7.java
@@ -2,10 +2,18 @@ package Task260122;
 
 public class Task7 {
     public static void main(String[] args) {
-        int[] array = {1, 0, 4, 3, 8, 9};
+        int[] array = {1, 2, 3, 4, 5, 3, 2, 1, 2, 6, 5, 4, 3, 3, 2, 1, 1, 3, 3, 3, 4, 2};  //1,0,4,3,8,9 //1,2,3,4,5,3,2,1,2,6,5,4,3,3,2,1,1,3,3,3,4,2
         int max = 0;
         int sum = 0;
         int num = 0;
+        if (array.length == 1) {
+            System.out.println("Максимально возможная сумма массива по условию задачи равна " + array[0]);
+            return;
+        }
+        if (array.length == 0) {
+            System.out.println("Массив незаполнен");
+            return;
+        }
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 if (array[j] > max) {
@@ -21,6 +29,10 @@ public class Task7 {
             } else if (num == 0) {
                 array[num] = 0;
                 array[num + 1] = 0;
+            } else if (array[num] == array[num + 1] && array[num + 1] > array[num + 2]) {
+                array[num] = 0;
+                array[num + 1] = 0;
+                array[num + 2] = 0;
             } else {
                 array[num] = 0;
                 array[num - 1] = 0;
@@ -30,3 +42,4 @@ public class Task7 {
         System.out.println("Максимально возможная сумма массива по условию задачи равна " + sum);
     }
 }
+
