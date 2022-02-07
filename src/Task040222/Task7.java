cbd46b3d57e7a27c4827dfffdc28b668a0bddfff
AlexandrLazarev <lazarev99@gmail.com>
2022-02-07 17:49:55 +0300

    changing

--- src/Task040222/Task7.java
+++ src/Task040222/Task7.java
@@ -5,11 +5,12 @@ import java.util.regex.Pattern;
 
 public class Task7 {
     public static void main(String[] args) {
-        String str = "Солнце, Антарктида, яблоня";
-        Pattern p = Pattern.compile("\bа.[^\s]*а\b");
+        String str = "Солнаце, Антарктида, яблоня а ара";
+        Pattern p = Pattern.compile("\\b[Аа]\\S*[Аа]\\b");
         Matcher m = p.matcher(str);
         while (m.find()) {
             System.out.println(m.group());
         }
+
     }
 }
