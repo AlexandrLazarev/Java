129a7a0c1e0a02c6af69b9ab865a50af31fec294
AlexandrLazarev <lazarev99@gmail.com>
2022-02-10 09:08:57 +0300

    changing

--- src/Task040222/Task9.java
+++ src/Task040222/Task9.java
@@ -1,13 +1,14 @@
 package Task040222;
 
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task9 {
     public static void main(String[] args) {
-        String str = "Номер карточки 5464-5484-8562-6859 нельзя сообщать";
+        String str = "Номер карточки 5464-5484-8562-6859 нельзя СообЩать";
         Pattern p = Pattern.compile("\\bсообщать\\b");
-        Matcher m = p.matcher(str);
+        Matcher m = p.matcher(str.toLowerCase());
         while (m.find()) {
             System.out.println("Искомое слово начинается с индекса " + m.start());
         }
