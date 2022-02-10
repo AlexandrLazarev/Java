c0d46eda7cf4b932ae7c0686810cfa402db189a0
AlexandrLazarev <lazarev99@gmail.com>
2022-02-10 08:59:03 +0300

    changing

--- src/Task040222/Task6Long.java
+++ src/Task040222/Task6Long.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class Task6Long {
     public static void main(String[] args) {
         String max = "";
-        String str = "Регулярные выражения — тема, которую программисты, даже опытные, зачастую откладывают на потом.";
-        Pattern p = Pattern.compile("[а-яА-Яa-zA-Z0-9]*");
+        String str = "Регулярные выражения — тема, которую программисты, даже опытные, зачастую откладывают на потом. фываавфыыф-ываавыф";
+        Pattern p = Pattern.compile("[а-яА-Яa-zA-Z0-9-]*");
         Matcher m = p.matcher(str);
         while (m.find()) {
             if (m.group().length() > max.length()) {
