89308c7cf8b7ae8515b473cbd1db11a12bca6e78
AlexandrLazarev <lazarev99@gmail.com>
2022-02-21 15:17:24 +0300

    changing

--- src/ItAcademy/Task220212/Showroom.java
+++ src/ItAcademy/Task220212/Showroom.java
@@ -1,5 +1,15 @@
 package ItAcademy.Task220212;
 
-public class Showroom extends CarFactory{
 
+public class Showroom {
+    private final Factory carFactory;
+    private final Service carService;
+
+    public Showroom(Factory carFactory, Service carService) {
+this.carFactory = carFactory;
+this.carService = carService;
+    }
 }
+
+
+
